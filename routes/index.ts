542ded0a59898dab6bcb5cbf6c1b9379d24e1cfa
Leefo <27723691@27723691.com>
2026-03-30 14:01:29 +0800

    Modify table name and request

--- routes/index.ts
+++ routes/index.ts
@@ -1,5 +1,5 @@
 import type { FastifyPluginAsync, FastifyReply, FastifyError } from "fastify";
-import userRoutes from "./user";
+import userRoutes from "./customer";
 const indexRoutes: FastifyPluginAsync = async (app, options) => {
   app.get("/", async (request, reply) => {
     return { hello: "world" };
